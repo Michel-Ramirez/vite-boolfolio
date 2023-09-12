3f8360d34b7c098e3cd4e030453f30d9d46ee55c
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-09-12 17:36:40 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid container">
+                <h1 class="navbar-brand" href="#">M R</h1>
+                <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button> -->
+                <!-- <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </div>
+                </div> -->
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header .navbar {
+    background-color: #9dcef2;
+}
+</style>
