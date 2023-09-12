96c20c8b2243748ea8c600484fc46a3feb7a1da3
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-09-12 17:59:03 +0200

    add axios call - not found

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+
+const enpoint = 'http://127.0.0.1:8000/api/projects/';
 
 export default {
-  components: { AppHeader }
+  components: { AppHeader },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProjects() {
+      axios.get(enpoint).then(res => { console.log(res.data) })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
 }
 </script>
 
 <template>
   <AppHeader />
   <main class="container my-3">
-
 
   </main>
 </template>
