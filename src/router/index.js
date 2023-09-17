c1b114d1e06ca885c582adc9e09ee4adcd041bc3
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-09-17 14:25:51 +0200

    add route for project detail

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 // importo le pagine
 
 import HomePage from "../pages/HomePage.vue";
-import ProjectPage from "../pages/ProjectPage.vue";
+import ProjectDetailPage from "../pages/ProjectDetailPage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
 //Creiamo il router
 
@@ -11,7 +11,11 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "home", component: HomePage },
-    { path: "/project", name: "project", component: ProjectPage },
+    {
+      path: "/projects/:id",
+      name: "project-detail",
+      component: ProjectDetailPage,
+    },
     { path: "/not-found", name: "not-found", component: NotFoundPage },
 
     // guarda se l'url matcha con qualunque altra cosa (qualunque altra cosa trovi '.*' //|// (.*)* -> qualunque query string) fai il redirct a: notFoundPage
